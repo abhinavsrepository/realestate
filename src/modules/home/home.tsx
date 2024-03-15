874a131dc937627ea0fe573656eb629ace4de262
Abhinav Singh <95268334+abhinavsrepository@users.noreply.github.com>
2024-03-15 17:58:47 +0530

    front page done

--- src/modules/home/home.tsx
+++ src/modules/home/home.tsx
@@ -1,10 +1,12 @@
 import Header from "@/shared/widgets/header/header"
 import Banner from "./features/banner"
+import Branding from "@/modules/home/features/branding"
 const Home = () => {
   return (
     <div>
       <Header/>
       <Banner/>
+      <Branding/>
     </div>
   )
 }

9e3c6cf287ea895e270ee36e2772e05bad649c59
Abhinav Singh <95268334+abhinavsrepository@users.noreply.github.com>
2024-08-01 00:22:19 +0530

    first

--- src/modules/home/home.tsx
+++ src/modules/home/home.tsx
@@ -2,6 +2,8 @@ import Header from "@/shared/widgets/header/header"
 import Banner from "./elements/banner"
 import Branding from "@/modules/home/elements/branding"
 import Benefits from "@/modules/home/elements/benefits"
+import FeatureHighlight from "./elements/feature.highlight"
+import Pricing from "./elements/pricing"
 const Home = () => {
   return (
     <div>
@@ -9,6 +11,9 @@ const Home = () => {
       <Banner/>
       <Branding/>
       <Benefits/>
+      <FeatureHighlight/>
+      <Pricing/>
+     
     </div>
   )
 }
